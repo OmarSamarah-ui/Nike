469eb4f86114bc0ed8412ee0356eb03af060d903
Omar Samara <OSamara@5ytechnology.com>
2024-07-17 16:24:58 +0300

    project lol

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = ({ label, iconUrl }) => {
+    return (
+        <button className='flex justify justify-centery gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
+            {label}
+            {iconUrl && <img src={iconUrl} alt='icon' className='ml-2 rounded-full w-5 h-5' />}
+        </button>
+    );
+};
+
+export default Button;
